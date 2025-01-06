4ac6728019bf3edb89b07e54e958678723331e59
Saiashishm7 <saiashishm1508@gmail.com>
2025-01-06 09:53:12 +0530

    commit

--- JavaScriptProjects/my-blog/src/Home.js
+++ JavaScriptProjects/my-blog/src/Home.js
@@ -1,20 +1,17 @@
 import { useState } from "react";
+import BlogList from "./bloglist";
 
 const Home = () => {
-  const [blogs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My Blog1", body: "Body of Blog1", author: "Cyrus", id: 1 },
     { title: "My Blog2", body: "Body of Blog2", author: "Zeus", id: 2 },
-    { title: "My Blog3", body: "Body of Blog3", author: "Apollo", id: 3 }
+    { title: "My Blog3", body: "Body of Blog3", author: "Cyrus", id: 3 }
   ]);
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by: {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Cyrus')} title="Cyrus's blogs" />
     </div>
   );
 }
