60ee5cd38d52814f62d5c480a3b7707d51a689dc
Saiashishm7 <saiashishm1508@gmail.com>
2025-01-27 09:46:45 +0530

    comit

--- JavaScriptProjects/my-blog/my-app/app/(tabs)/index.jsx
+++ JavaScriptProjects/my-blog/my-app/app/(tabs)/index.jsx
@@ -1,39 +1,60 @@
-import { View, Text, StyleSheet, ImageBackground } 
-from 'react-native';
-import React from 'react';
-import coffeeImg from "@/assets/images/coffeee.png";
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={coffeeImg}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <Text style={styles.text}>Coffee Shop</Text>
-      </ImageBackground>
-    </View>
-  );
-}
-
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    flex: 1,
-    justifyContent: 'center',
-  },
+import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'; 
+import React from 'react'; 
+import icedCoffeeImg from "../../assets/images/car.png"; // Adjust path as needed 
+import { Link } from 'expo-router'; 
+ 
+const App = () => { 
+  return ( 
+    <View style={styles.container}> 
+      <ImageBackground 
+        source={icedCoffeeImg} 
+        resizeMode="cover" 
+        style={styles.image} 
+      > 
+        <Text style={styles.text}>High On Cars</Text> 
+        <Link href="/contact" style={{ marginHorizontal: 'auto' }} asChild> 
+          <Pressable style={styles.button}> 
+            <Text style={styles.buttonText}>Contact Us</Text> 
+          </Pressable> 
+        </Link> 
+      </ImageBackground> 
+    </View> 
+  ); 
+}; 
+ 
+export default App; 
+ 
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    flexDirection: 'column', 
+  }, 
+  text: { 
+    color: 'white', 
+    fontSize: 42, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+    padding: 10, 
+  }, 
+  image: { 
+    width: '100%', 
+    height: '100%', 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  }, 
+  button: { 
+    marginTop: 20, 
+    backgroundColor: 'rgba(0, 0, 0, 0.7)', 
+    paddingVertical: 10, 
+    paddingHorizontal: 20, 
+    borderRadius: 5, 
+  }, 
+  buttonText: { 
+    color: 'white', 
+    fontSize: 16, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+  }, 
 });
