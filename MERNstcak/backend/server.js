77c50c2c0ddb0a6f0960d177f7617b35ebedbe50
Saiashishm7 <saiashishm1508@gmail.com>
2025-02-10 09:20:02 +0530

    commit

---
+++ MERNstcak/backend/server.js
@@ -0,0 +1,62 @@
+// require('dotenv').config()
+
+// const express = require('express')
+
+// const mongoose=require('mongoose')
+
+// const app=express()
+
+// const workoutRoutes=require('./routes/workouts')
+
+// app.use(express.json())
+
+
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method)
+//     next()
+//   })
+  
+// //   // routes
+// app.use('/api/workouts', workoutRoutes)
+
+// mongoose.connect(process.env.MONG_URI)
+// .then(() => {
+//     app.listen(process.env.PORT, () => {
+//       console.log('listening on port', process.env.PORT)
+//     })
+//   })
+//   .catch((error) => {
+//       console.log(error)
+//   })
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const workoutRoutes = require('./routes/workouts');
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
+
+// Connect to MongoDB
+const uri = process.env.MONG_URI;
+const port = process.env.PORT || 4000;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log('listening on port', port);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
