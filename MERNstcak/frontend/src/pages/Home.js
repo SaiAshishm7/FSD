a6df368b22d3e738997dd280de00f781e99cf419
Saiashishm7 <saiashishm1508@gmail.com>
2025-02-28 09:35:04 +0530

    index

--- MERNstcak/frontend/src/pages/Home.js
+++ MERNstcak/frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('http://localhost:3000/api/workouts');
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <p key={workout._id}>{workout.title}</p>
+        ))}
+      </div>
     </div>
   );
 };
