bc9941fa23b26f99bdb134c758bf742e0912821b
Saiashishm7 <saiashishm1508@gmail.com>
2025-02-28 09:18:50 +0530

    commit

--- MERNstcak/frontend/frontend/src/App.js
+++ MERNstcak/frontend/frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Workout App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
